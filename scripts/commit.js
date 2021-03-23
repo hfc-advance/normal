fc20d29430ca248c9cba8183749b3d10f3ef73bd
崔海峰 <haifeng.cui@china.zhaogang.com>
2021-03-23 13:59:57 +0800

    feat(*): add index.html

---
+++ scripts/commit.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const del = require('del')
+const fsEx = require('fs-extra')
+const execa = require('execa')
+const targetDir = path.resolve(__dirname, '../../hfci.github.io/docs')
+const gitCWD = path.resolve(targetDir, '../')
+
+async function commit () {
+  await del([targetDir], { force: true })
+  await fsEx.copy(path.resolve(__dirname, '../build'), targetDir)
+
+  await execa(
+    'git',
+    ['add', '.'],
+    { stdio: 'inherit', cwd: gitCWD }
+  )
+  await execa(
+    'git',
+    ['commit', '-m', 'auto'],
+    { stdio: 'inherit', cwd: gitCWD }
+  )
+  await execa(
+    'git',
+    ['push'],
+    { stdio: 'inherit', cwd: gitCWD }
+  )
+}
+
+commit()
+
