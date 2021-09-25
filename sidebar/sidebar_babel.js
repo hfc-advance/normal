917c7e9c65057d8711443e63cde2fea54bfade52
崔海峰 <cuihaifeng1919@hotmail.com>
2021-09-25 23:29:32 +0800

    Ifeat(*): 修改markdown样式

--- sidebar/sidebar_babel.js
+++ sidebar/sidebar_babel.js
@@ -2,67 +2,67 @@ module.exports = [
   {
     type: 'category', label: '',
     items: ['enginee/findConfig/getEngineeConfigFile'],
-    collapsed: false
+    collapsed: true
   },
-  { type: 'category', label: 'Babel', items: ['enginee/babel/babel', 'enginee/babel/babelPlugins', 'enginee/babel/babelVersions', 'enginee/babel/babelPluginAna', 'enginee/babel/babelBase', 'enginee/babel/babelMacros'], collapsed: false },
+  { type: 'category', label: 'Babel', items: ['enginee/babel/babel', 'enginee/babel/babelPlugins', 'enginee/babel/babelVersions', 'enginee/babel/babelPluginAna', 'enginee/babel/babelBase', 'enginee/babel/babelMacros'], collapsed: true },
   {
     type: 'category',
     label: 'webpack',
-    collapsed: false,
+    collapsed: true,
     items: ['enginee/webpack/webpackLoader', 'enginee/webpack/webpackOftenFunctional', 'enginee/webpack/webpackCompileProcess', 'enginee/webpack/webpackTapable', 'enginee/webpack/webpackModuleFederation', 'enginee/webpack/webpackAssetModule', 'enginee/webpack/webpack5Changelog', 'enginee/webpack/webpackOptimization', 'enginee/webpack/webpackImportantMethod', 'enginee/webpack/webpackOftenPkg', 'enginee/webpack/webpackFood', 'enginee/webpack/webpackComponentNotice', 'enginee/webpack/webpackPlugin', 'enginee/webpack/webpackUsefulPlugin'],
   },
   {
     type: 'category',
     label: 'npm',
-    collapsed: false,
+    collapsed: true,
     items: ['enginee/npm/packagejsonKeys', 'enginee/npm/npmShell', 'enginee/npm/.npmrc', 'enginee/npm/node_modules'],
   },
   {
     type: 'category',
     label: 'git',
-    collapsed: false,
+    collapsed: true,
     items: ['enginee/git/gitNotify'],
   },
   {
     type: 'category',
     label: 'typescript',
-    collapsed: false,
+    collapsed: true,
     items: ['enginee/typescript/typescriptBseconfig'],
   },
   {
     type: 'category',
     label: 'postcss',
-    collapsed: false,
+    collapsed: true,
     items: ['enginee/postcss/postcssPlugin'],
   },
   {
     type: 'category',
     label: 'prettier',
-    collapsed: false,
+    collapsed: true,
     items: ['enginee/prettier/prettierPlugin'],
   },
   {
     type: 'category',
     label: '配置项',
-    collapsed: false,
+    collapsed: true,
     items: ['enginee/projectConfigItem', 'enginee/projectUpdatePac'],
   },
   {
     type: 'category',
     label: 'stylus',
-    collapsed: false,
+    collapsed: true,
     items: ['enginee/stylus/stylusUsefulPlugin'],
   },
   {
     type: 'category',
     label: 'eslint',
-    collapsed: false,
+    collapsed: true,
     items: ['enginee/eslint/eslintPlugins'],
   },
   {
     type: 'category',
     label: 'lerna',
-    collapsed: false,
+    collapsed: true,
     items: ['enginee/lerna/lernaApi']
   }
 ]
